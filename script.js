7cfeb53354cacc6a37dd77f743e3cbacf20c2887
Powdacious <116194683+Powdacious@users.noreply.github.com>
2023-12-16 22:41:39 +0000

    Update script.js
    adding session storage for users data input on ui

--- script.js
+++ script.js
@@ -9,6 +9,37 @@ const features = [
 const valueOptions = ['a beer', 'a week\'s wages', 'a holiday', 'their car', 'their house'];
 const costOptions = ['a beer', 'a week\'s wages', 'a holiday', 'a car', 'a house'];
 
+// Function to submit feature entered by the user
+function submitFeature() {
+    const featureName = document.getElementById('featureName').value;
+    const value = document.getElementById('value').value;
+    const cost = document.getElementById('cost').value;
+
+    // Create an object to represent the feature
+    const newFeature = {
+        name: featureName,
+        value: value,
+        cost: cost,
+    };
+
+    // Get the existing features from sessionStorage
+    let featuresFromSession = JSON.parse(sessionStorage.getItem('features')) || [];
+
+    // Add the new feature to the array
+    featuresFromSession.push(newFeature);
+
+    // Store the updated features in sessionStorage
+    sessionStorage.setItem('features', JSON.stringify(featuresFromSession));
+
+    // Refresh the table to display the updated features
+    renderTable();
+}
+
+// Function to calculate priority based on value and cost
+function calculatePriority(value, cost) {
+    return value * cost;
+}
+
 // Function to render the table
 function renderTable() {
     const table = document.getElementById('featureTable');
@@ -23,8 +54,11 @@ function renderTable() {
         headerRow.appendChild(th);
     });
 
+    // Get the features from sessionStorage
+    const featuresFromSession = JSON.parse(sessionStorage.getItem('features')) || [];
+
     // Create data rows
-    features.forEach(feature => {
+    featuresFromSession.forEach(feature => {
         const row = table.insertRow();
         const priority = calculatePriority(feature.value, feature.cost); // Calculate priority
 
@@ -38,11 +72,6 @@ function renderTable() {
         const priorityCell = row.insertCell();
         priorityCell.textContent = priority;
     });
-}
-
-// Function to calculate priority based on value and cost
-function calculatePriority(value, cost) {
-    return value * cost;
 }
 
 // Call the renderTable function to initially populate the table
